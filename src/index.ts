cd8bbb7f0613dfa150b229d12f277f0e2822086a
santhitak <63070175@kmitl.ac.th>
2022-12-05 04:28:36 +0700

    feat: getlend and getcategories

--- src/index.ts
+++ src/index.ts
@@ -330,6 +330,26 @@ app.post("/favorites/:productId", async (req, res) => {
   res.json(updateisFavorite);
 });
 
+app.get("/lends/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const getItems = await prisma.userLent.findMany({
+    where: {
+      userId: Number(userId),
+    },
+  });
+  res.json(getItems);
+});
+
+app.get("/categories/:productId", async (req, res) => {
+  const { productId } = req.params;
+  const getItems = await prisma.categoriesOnProduct.findMany({
+    where: {
+      productId: Number(productId),
+    },
+  });
+  res.json(getItems);
+});
+
 const server = app.listen(3000, () =>
   console.log(`
 🚀 Server ready at: http://localhost:3000`)
